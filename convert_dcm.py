242d77f22f2d7ffb3286abb5b66048d8699429a8
julianoce <julianoce@hotmail.com>
2021-11-10 20:31:53 -0300

    changes

--- convert_dcm.py
+++ convert_dcm.py
@@ -5,13 +5,14 @@ from PIL import Image
 import numpy as np
 import glob
 import tensorflow as tf
-
+from skimage.transform import resize
+IMG_PX_SIZE = 512
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-train__covid_txt = open("/home/julia/Downloads/dados_covid/train_covid.txt",'r')
-train_txt_non_covid = open("/home/julia/Downloads/dados_covid/train_non_covid.txt",'r')
-train_txt = open("/home/julia/Downloads/dados_covid/train.txt",'r')
+train__covid_txt = open("/home/julia/Downloads/dados_covid/val_covid.txt",'r')
+train_txt_non_covid = open("/home/julia/Downloads/dados_covid/val_non_covid.txt",'r')
+train_txt = open("/home/julia/Downloads/dados_covid/val.txt",'r')
 
 train_covid = []
 train_non_covid = []
@@ -37,7 +38,7 @@ values = [i for i in list(range(2))]
 classes = dict(zip(keys, values))
 
 recordFileNum = 0
-recordFileName = ("train.tfrecords-%.3d" % recordFileNum)
+recordFileName = ("val.tfrecords-%.3d" % recordFileNum)
 writer = tf.io.TFRecordWriter(recordPath + recordFileName)
 
 for filename in train_filename:
@@ -51,13 +52,17 @@ for filename in train_filename:
     ds = pydicom.dcmread(filename)
     image = ds.pixel_array.astype(float)
     image = (np.maximum(image, 0) / image.max()) * 255.0
-    image = Image.fromarray(np.uint8(image)).convert('RGB')
-    img_raw = image.tobytes()
+    image = resize(image, (IMG_PX_SIZE, IMG_PX_SIZE), anti_aliasing=True)
+    image = image.reshape((image.shape[0], image.shape[1], 1))
+    print(image.shape)
+    image_raw = image.flatten().tostring()
+    print(image.shape)
+
     example = tf.train.Example(features=tf.train.Features(feature={
-    'height': _int64_feature(224),
-    'width': _int64_feature(224),
-    'depth': _int64_feature(3),
-    "img_raw": tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw])),
+    'height': _int64_feature(512),
+    'width': _int64_feature(512),
+    'depth': _int64_feature(1),
+    "image_raw": tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_raw])),
     "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))}))
 writer.write(example.SerializeToString())
 writer.close()
